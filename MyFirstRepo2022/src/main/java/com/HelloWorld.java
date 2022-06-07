9eaa4cc6d335fba6b26a4124c33775b01f527b38
Oleggithub2022 <olegmirkyivstar@gmail.com>
2022-06-07 21:28:52 +0300

    first commit

---
+++ MyFirstRepo2022/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Олег Мирошниченко on 07.06.2022.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
